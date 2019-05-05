b17456a7385c8392a901cb5d414a6b2de75acf01
xieyucheng <648547525@qq.com>
2019-05-05 21:47:19 +0800

    add al

---
+++ test1.py
@@ -0,0 +1,11 @@
+import binascii
+import hashlib
+import struct
+import wallet
+
+# 测试存正
+# wallet.record('helloworld')
+# 测试脚本
+# wallet.transfer(['1118hfRMRrJMgSCoV9ztyPcjcgcMZ1zThvqRDLUw3xCYkZwwTAbJ5o=0.6'])
+
+wallet.utxo_test(0,40743502879916033,())
